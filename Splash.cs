2436e8f7ae60c69bbcc6e9465a14563027cbfa2b
juicyj201 <80794017+juicyj201@users.noreply.github.com>
2021-06-27 16:44:01 +0200

    Add Splash Screen

---
+++ Splash.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LearningGame1
+{
+    public partial class Splash : Form
+    {
+        public Splash()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
